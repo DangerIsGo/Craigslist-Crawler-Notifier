9fb27dc0c3564c7c90dbc14a0308a00a42c53090
Devon <DangerIsGo@gmail.com>
2014-03-10 00:30:50 -0400

    Initial Commit

---
+++ CraigslistCrawlerNotifier/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace CraigslistCrawlerNotifier
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.listBox_Results = new System.Windows.Forms.ListBox();
+            this.txtBx_SearchString = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(193, 465);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(139, 23);
+            this.btn_Search.TabIndex = 0;
+            this.btn_Search.Text = "Start Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // listBox_Results
+            // 
+            this.listBox_Results.FormattingEnabled = true;
+            this.listBox_Results.Location = new System.Drawing.Point(12, 12);
+            this.listBox_Results.Name = "listBox_Results";
+            this.listBox_Results.Size = new System.Drawing.Size(517, 420);
+            this.listBox_Results.TabIndex = 1;
+            // 
+            // txtBx_SearchString
+            // 
+            this.txtBx_SearchString.Location = new System.Drawing.Point(12, 439);
+            this.txtBx_SearchString.Name = "txtBx_SearchString";
+            this.txtBx_SearchString.Size = new System.Drawing.Size(517, 20);
+            this.txtBx_SearchString.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 499);
+            this.Controls.Add(this.txtBx_SearchString);
+            this.Controls.Add(this.listBox_Results);
+            this.Controls.Add(this.btn_Search);
+            this.Name = "MainForm";
+            this.Text = "Craigslist Search Notifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.ListBox listBox_Results;
+        private System.Windows.Forms.TextBox txtBx_SearchString;
+    }
+}
+
---
+++ CraigslistCrawlerNotifier/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+namespace CraigslistCrawlerNotifier
+{
+    public partial class MainForm : Form
+    {
+        List<string> ParagraphTagList;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.ParagraphTagList = new List<string>();
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            string SearchQuery = txtBx_SearchString.Text.Trim().Replace(" ", "+");
+
+            string UrlQuery = string.Concat("http://boston.craigslist.org/search/?sort=date&catAbb=sss&query=", SearchQuery);
+
+            WebRequest Request = WebRequest.Create(UrlQuery);
+            WebResponse Response = Request.GetResponse();
+            Stream PageData = Response.GetResponseStream();
+            string PageHtml = string.Empty;
+            using (StreamReader sr = new StreamReader(PageData))
+            {
+                PageHtml = sr.ReadToEnd();
+            }
+
+            // Match paragraph contents
+            Match Content = Regex.Match(PageHtml, @"<p.*/p>", RegexOptions.None);
+
+            int start = Content.Value.IndexOf("<p");
+            int end = Content.Value.IndexOf("/p>");
+
+            string FullPage = Content.Value;
+
+            string SubSet = String.Empty;
+
+            while (start > -1 && end > -1)
+            {
+                SubSet = FullPage.Substring(start, end + 3).Trim();
+                FullPage = FullPage.Substring(end + 3).Trim();
+
+                if (!string.IsNullOrEmpty(SubSet))
+                {
+                    this.ParagraphTagList.Add(SubSet);
+                }
+
+                start = FullPage.IndexOf("<p");
+                end = FullPage.IndexOf("/p>");
+            }
+
+            foreach (string item in this.ParagraphTagList)
+            {
+                this.ProcessParagraphItem(item);
+            }
+        }
+
+        private void ProcessParagraphItem(string data)
+        {
+            //<span class="pl"> = Item
+            //<span class="price"> = Price
+            //<span class="date"> = Date
+            //<span class="pnr"> = Location
+            Match date = Regex.Match(data, "<span\\s+class\\s*=\\s*\"date\"\\s*>[a-zA-Z0-9\\s]+<\\s*/\\s*span\\s*>");
+            Match price = Regex.Match(data, "<span\\s+class\\s*=\\s*\"\\s*price\\s*\"\\s*>&#x0024;[0-9\\.]*<\\s*/\\s*span\\s*>");
+        }
+    }
+}
